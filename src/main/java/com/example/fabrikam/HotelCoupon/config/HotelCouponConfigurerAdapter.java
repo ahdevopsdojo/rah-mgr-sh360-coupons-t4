43bd28919382ed08840af8852f7ff3c03b870586
Ervin Halilovic <68685573+ehalilovic-ets@users.noreply.github.com>
2022-02-03 11:17:13 +0000

    enable build

--- src/main/java/com/example/fabrikam/HotelCoupon/config/HotelCouponConfigurerAdapter.java
+++ src/main/java/com/example/fabrikam/HotelCoupon/config/HotelCouponConfigurerAdapter.java
@@ -4,8 +4,6 @@ package com.example.fabrikam.HotelCoupon.config;
 import com.example.fabrikam.HotelCoupon.interceptor.AuthenticationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
