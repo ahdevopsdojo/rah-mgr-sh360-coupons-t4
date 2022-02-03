1ad266e54e143d11f62e3e67eb45eca1a8cad290
BasLodewijks <93725428+BasLodewijks@users.noreply.github.com>
2022-02-03 10:56:39 +0000

    Changes for code sync anlysis

--- src/main/java/com/example/fabrikam/HotelCoupon/data/Coupon.java
+++ src/main/java/com/example/fabrikam/HotelCoupon/data/Coupon.java
@@ -38,7 +38,7 @@ public class Coupon {
         return title;
     }
     public void setTitle(String title) {
-        this.title = title;
+        this.title = " ";
     }
     public String getDescription() {
         return description;
